ec435ebc982a62fa19cf1d3f6cd0020e704c4396
OD3ZZA <30643662+od3zza@users.noreply.github.com>
2025-07-31 23:16:08 -0300

    Update images.js

--- images.js
+++ images.js
@@ -10,35 +10,6 @@ window.FlowSketchImages = [
     image: "images/artwork_1754014296663.png"
   },
 
-  {
-    title: "será que vai?",
-    artist: "od3zza",
-    date: "2025-08-01T01:48:49.102Z",
-    points: 9500,
-    image: "images/artwork_1754012929102.png"
-  },
-
-  {
-    title: "9try",
-    artist: "od3zza", 
-    date: "2025-07-31T12:00:00",
-    points: 5729,
-    image: "images/artwork_1753992909727.png"
-  },
-  {
-    title: "7try",
-    artist: "od3zza",
-    date: "2025-07-31T11:00:00", 
-    points: 3274,
-    image: "images/artwork_1753992319879.png"
-  },
-  {
-    title: "6try",
-    artist: "od3zza",
-    date: "2025-07-31T10:00:00",
-    points: 1664,
-    image: "images/artwork_1753991414884.png"
-  }
 ];
 
 // Versão do arquivo para controle de cache
